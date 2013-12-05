6c6c46d8a571156f5e0d8d754168bc00b06b12fb
jhoell1 <jhoell@umich.edu>
2013-12-05 15:58:44 -0500

    pcl shit=>doesnt work yet

---
+++ pclManip/CMakeFiles/pclManipTest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/pclManipTest.dir/pclManipTest.cpp.o"
+  "CMakeFiles/pclManipTest.dir/pclManipulator.cpp.o"
+  "pclManipTest.pdb"
+  "pclManipTest"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/pclManipTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
